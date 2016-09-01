729710a3bc5c39a27d1d056b63b257d4adcc44ff
requires.io <support@requires.io>
2016-09-01 23:57:32 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ test_requires = [
     'pytest-cov==2.3.1',
     'pytest-benchmark==3.0.0',
     'pylama==7.0.9',
-    'pyflakes==1.2.3',
+    'pyflakes==1.3.0',
     'pep257==0.7.0',
     'pep8==1.7.0',
     'mccabe==0.5.2',
