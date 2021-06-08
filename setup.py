c85169c61b69d5c6e005c944b9c9a99ac2a29c88
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2021-06-08 12:14:02 +0200

    Move package configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -17,63 +17,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with matchbox.  If not, see <http://www.gnu.org/licenses/>.
 """matchbox's installation module."""
+from setuptools import setup
 
-import os
-from setuptools import setup, find_packages
-
-here = os.path.dirname(__file__)
-
-
-def read(fname):
-    """
-    Read filename.
-
-    :param str fname: name of a file to read
-    """
-    return open(os.path.join(here, fname)).read()
-
-
-requirements = []
-
-test_requires = [
-    "pytest",
-    "pytest-cov",
-    "pytest-benchmark",
-]
-
-extras_require = {"docs": ["sphinx"], "tests": test_requires}
-
-setup(
-    name="matchbox",
-    version="1.0.0",
-    description=(
-        "Abstraction layer for creating hash maps to speed up extracting" + " subsets out of objects in collection"
-    ),
-    long_description=(read("README.rst") + "\n\n" + read("CHANGES.rst")),
-    keywords="python index match",
-    author="Grzegorz Sliwinski",
-    author_email="g.sliwinski@clearcode.cc",
-    url="https://github.com/ClearcodeHQ/matchbox",
-    license="LGPLv3+",
-    python_requires=">=3.7",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    install_requires=requirements,
-    tests_require=test_requires,
-    test_suite="tests",
-    include_package_data=True,
-    zip_safe=False,
-    extras_require=extras_require,
-)
+setup()
